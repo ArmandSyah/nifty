4e63416462ebf7f328e9724b92477b307e066326
ArmandSyah <armand.syahtama@gmail.com>
2018-04-19 19:35:01 -0400

    added analyze function

--- nifty.py
+++ nifty.py
@@ -1,5 +1,6 @@
 import string
 import pprint
+from functools import reduce
 from nltk.corpus import stopwords
 from wordentry import WordEntry
 
@@ -16,6 +17,20 @@ def parse_review(rating: int, words: list):
             word_entries[word] = WordEntry(word, rating)
 
 
+def analyze():
+    while True:
+        sentence = input('Type something: ')
+        sentence = [word.lower().translate(punctuation_clean_up)
+                    for word in sentence.split() if word not in english_stop_words]
+        total, useful_words = 0, 0
+        for word in sentence:
+            if word in word_entries:
+                total += word_entries[word].get_average()
+                useful_words += 1
+        print(
+            f"This review has an average value of {total / useful_words}")
+
+
 def main():
     with open('movieReviews.txt') as reviews:
         for line in reviews:
@@ -28,6 +43,7 @@ def main():
     for _, v in word_entries.items():
         print(
             f"Word: {v.word}, Total: {v.total_score}, Frequency: {v.frequency}, Average: {v.get_average()}")
+    analyze()
 
 
 if __name__ == '__main__':
